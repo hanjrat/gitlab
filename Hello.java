75217008ccb422be6b7aee9a85eaa8223c689084
Tahir Hanjra <hanjrat@go.stockton.edu>
2020-11-27 22:21:49 -0500

    Revert "Oops, we didn't want this commit"
    This reverts commit 967dc7f0bdc95eae1db3abfaaad7a5f05fdf2959.

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello
 {
  public static void main( String argv[] )
  {
- // This is an unwanted but staged comment
  String name = "World";
  if ( argv.length != 0 )
  {
@@ -11,4 +10,3 @@ public class Hello
  System.out.println( "Hello, " + name + "!" );
  }
 }
-
